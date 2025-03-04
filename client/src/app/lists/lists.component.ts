f6326814cc73248a5a677789b0d312b199d2349c
R2 <rfayko@raadsinc.com>
2025-03-04 17:17:56 -0500

    Cleared pagination result

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { LikesService } from '../_services/likes.service';
 import { Member } from '../_models/member';
 import { FormsModule } from '@angular/forms';
@@ -13,7 +13,7 @@ import { PageChangedEvent, PaginationModule } from 'ngx-bootstrap/pagination';
   templateUrl: './lists.component.html',
   styleUrl: './lists.component.css'
 })
-export class ListsComponent implements OnInit {
+export class ListsComponent implements OnInit, OnDestroy {
   likesService = inject(LikesService);
   predicate = "liked";
   pageNumber = 1
@@ -35,11 +35,15 @@ export class ListsComponent implements OnInit {
     this.likesService.getLikes(this.predicate, this.pageNumber, this.pageSize);
   }
 
-    pageChanged(event: PageChangedEvent) {
-      if (this.pageNumber != event.page) {
-        this.pageNumber = event.page;
-        this.loadLikes();
-      }
+  pageChanged(event: PageChangedEvent) {
+    if (this.pageNumber != event.page) {
+      this.pageNumber = event.page;
+      this.loadLikes();
     }
+  }
+
+  ngOnDestroy(): void {
+    this.likesService.paginatedResult.set(null);
+  }
 
 }
